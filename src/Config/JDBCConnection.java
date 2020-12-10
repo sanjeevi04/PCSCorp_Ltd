1d1a32739c3434765607cf9521b9c8c2b44f1798
sanjeevi04 <71927387+sanjeevi04@users.noreply.github.com>
2020-12-11 00:08:54 +0530

    Revert "Commit"
    This reverts commit 7ebe319a099a413854cf135a6696106807e92674.

--- src/Config/JDBCConnection.java
+++ src/Config/JDBCConnection.java
@@ -1,18 +1,5 @@
 package Config;
 
-
-import java.sql.*;
-
 public class JDBCConnection {
-  public static Connection getDBConnection() throws ClassNotFoundException,SQLException{
-	   String url="jdbc:mysql://localhost:3306/pcsdb";
-	   String username="root";
-	   String password="niit@123";
-	   
-	   
-	   Class.forName("com.mysql.jdbc.Driver");
-	   Connection conn=DriverManager.getConnection(url, username, password);
-	   return conn;
-	   
-  }
+
 }
